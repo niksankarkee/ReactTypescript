fd757e9ec3fe8c9bc0ac2efdcbe7dd18f85b8602
unknown <karkeeniksan@gmail.com>
2021-12-07 13:26:51 +0900

    good example for reducer setup with typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,7 @@
 import ReactDOM from "react-dom";
-import UserSearch from "./refs/UserSearch";
 
 const App = () => {
-  return (
-    <div>
-      <UserSearch />
-    </div>
-  );
+  return <div></div>;
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
